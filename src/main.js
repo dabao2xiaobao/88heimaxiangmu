d24def76e8347ca302bcd8134d2211ef7869892d
dabao2xiaobao <mimawowangle0.0>
2019-11-13 11:57:44 +0800

    axios配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototypo.$axios = axios // 共享axios所有实例上使用
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
