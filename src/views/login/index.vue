1bd62ef63f8b5e78f9415a16ea58de0cadb1ba07
dabao2xiaobao <mimawowangle0.0>
2019-11-13 11:23:07 +0800

    数据绑定及自动校验规则

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,22 +6,22 @@
       <div class='title'>
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-      <!-- 表单 -->
-      <el-form style="margin-top:30px">
+      <!-- 表单   表单数据对象      验证规则对象 -->
+      <el-form :model="loginForm" :rules="loginRules" style="margin-top:30px">
         <!-- 一个表单域就是一个form-item -->
-        <el-form-item>
+        <el-form-item prop="mobile">
           <!-- 放置表单组件  -->
           <!-- 手机号 -->
-          <el-input></el-input>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机哈"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- 验证码 和发送验证码 -->
-          <el-input style='width:280px'></el-input>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style='width:280px'></el-input>
           <el-button style="float:right" plain>发送验证码</el-button>
         </el-form-item>
-         <el-form-item>
+         <el-form-item prop="checked">
            <!-- 勾选同意框 -->
-           <el-checkbox>我已阅读并同意用户协议及条款</el-checkbox>
+           <el-checkbox v-model="loginForm.checked">我已阅读并同意用户协议及条款</el-checkbox>
          </el-form-item>
          <el-form-item>
            <el-button style="width:100%" type='primary'>登录</el-button>
@@ -33,7 +33,43 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        // 要校验的整个的表单数据
+        // 手机号
+        mobile: '',
+        // 验证码
+        code: '',
+        // 是否选中
+        checked: false
+      },
+      //   校验规则对象
+      loginRules: {
+        //   键值对  键 要检验的字段名  val 数组,可以使一条或者多条
+        mobile: [
+          { required: true, message: '请输入您的手机号' },
+          { patter: /^1[3456789]\d{9}$/, message: '请输入真确的手机号' }
+        ],
+        code: [
+          { required: true, message: '请输入您验证码' },
+          { patter: /^\d{6}$/, message: '请输入六位数字' }
+        ],
+        checked: [
+          // 自定义校验函数   rule当前规则 value当前的值(checked) callback回调函数
+          { validator: function (rule, value, callback) {
+            if (value) {
+              // 如果是true就是选中了,直接通过
+              callback() // 直接执行callback
+            } else {
+              // 没有选中就不通过,给一个错误信息
+              callback(new Error('不点√你看你妈呦')) // 也直接执行callback
+            }
+          } }
+        ]
+      }
+    }
+  }
 }
 </script>
 
