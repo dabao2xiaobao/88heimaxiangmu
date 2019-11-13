41df302fd99e4cf9c72d30e913e9b621e2a8a6f6
dabao2xiaobao <mimawowangle0.0>
2019-11-13 15:27:33 +0800

    主页布局及左侧导航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>
-    主页
-  </div>
+  <!-- 布局 -->
+  <el-container>
+    <!-- 左右布局 -->
+    <el-aside class="left" style="width:220px">
+      <div class="menu-titie" style="background-color:#2e2f32">
+        <img src="../../assets/img/logo_admin.png" alt="">
+      </div>
+      <!-- 左侧菜单 -->
+      <el-menu style="width:221px" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+        <!-- 一级菜单 -->
+        <el-menu-item>首页</el-menu-item>
+        <!-- 二级菜单 -->
+        <el-submenu>
+          <!-- 具名插槽 -->
+          <template slot="title">内容管理</template>
+          <el-menu-item>文章文章</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>素材列表</el-menu-item>
+          <el-menu-item>评论列表</el-menu-item>
+        </el-submenu>
+         <!-- 二级菜单 -->
+        <el-submenu>
+          <template slot="title">粉丝管理</template>
+          <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>粉丝画像</el-menu-item>
+          <el-menu-item>粉丝列表</el-menu-item>
+        </el-submenu>
+       <!-- 一级菜单 -->
+        <el-menu-item>账户信息</el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>头部内容</el-header>
+      <el-main>
+        <!-- 中间内容 -->
+        <!-- 二级路由容器 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -13,5 +51,17 @@ export default {
   }
 }
 </script>
-<style>
-</style>
+<style lang='less' scoped>
+.left {
+  height: 100vh;
+  background-color: #353b4e;
+  overflow: hidden;
+.menu-titie {
+  padding: 15px 0;
+  text-align: center;
+  img {
+    height: 30px;
+  }
+}
+}
+</style>>
