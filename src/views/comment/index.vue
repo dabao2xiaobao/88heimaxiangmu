c0673fe4dd8f12a388c6288e0a8be82157d3777f
dabao2xiaobao <mimawowangle0.0>
2019-11-19 08:42:32 +0800

    修改评论状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -28,7 +28,8 @@
             <el-switch
               v-model="scope.row.comment_status"
               active-color="#13ce66"
-              inactive-color="#ff4949">
+              inactive-color="#ff4949"
+              @change='onStatusChange(scope.row)'>
             </el-switch>
           </template>
         </el-table-column>
@@ -73,6 +74,27 @@ export default {
         this.articles = res.data.data.results
       }).catch(err => {
         console.log(err, '获取失败')
+      })
+    },
+    onStatusChange (article) {
+      this.$axios({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: article.id.toString()
+        },
+        data: {
+          allow_comment: article.comment_status
+        }
+      }).then(res => {
+        console.log(res)
+        this.$message({
+          type: 'success',
+          message: `${article.comment_status ? '启用' : '关闭'} 成功`
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('操作失败')
       })
     }
   }
